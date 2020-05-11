4c2a3d12c62b2a7f02bfe87c702ad39a418de9d1
Martín M <skydiver@users.noreply.github.com>
2020-05-10 22:07:56 -0300

    working on update command

--- src/LaravelCheckUpdatesServiceProvider.php
+++ src/LaravelCheckUpdatesServiceProvider.php
@@ -3,58 +3,16 @@
 namespace Skydiver\LaravelCheckUpdates;
 
 use Illuminate\Support\ServiceProvider;
+use Skydiver\LaravelCheckUpdates\Console\CheckUpdates;
 
 class LaravelCheckUpdatesServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-check-updates');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-check-updates');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('laravel-check-updates.php'),
-            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-check-updates'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laravel-check-updates'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-check-updates'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
+            $this->commands([
+                CheckUpdates::class
+            ]);
         }
-    }
-
-    /**
-     * Register the application services.
-     */
-    public function register()
-    {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-check-updates');
-
-        // Register the main class to use with the facade
-        $this->app->singleton('laravel-check-updates', function () {
-            return new LaravelCheckUpdates;
-        });
     }
 }
